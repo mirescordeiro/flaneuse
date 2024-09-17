625ba1cf49e75b3225e364409a4ee3fee15cb9fc
Tamires Cordeiro <mirescordeiro@gmail.com>
2024-09-16 23:28:41 -0300

    [#1] feat(product): create product page and add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,8 @@ export const routes: Routes = [
     path: "",
     loadComponent: () => import("./features/shop/pages/home/home.component").then((m) => m.HomeComponent),
   },
+  {
+    path: "product-detail",
+    loadComponent: () => import("./features/product/pages/product/product.component").then((m) => m.ProductComponent),
+  },
 ];
